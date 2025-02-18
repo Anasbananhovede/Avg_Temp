4bc7cfd13bc8e3a637e421f55fad01fa5463fe93
Anasbananhovede <anas24092003978@gmail.com>
2025-02-18 12:43:40 +0100

    Add project files.

---
+++ Avg_Temp/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Avg_Temp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] dayTemp = { 7, 4, 5, 5, 4, 4, 4 };//defines the data
+            double sum = 0;//gives a sum variable for adding up
+            double maxTemp = dayTemp[0];
+            double minTemp = dayTemp[0];
+            for (int i = 0; i < dayTemp.Length; i++) //starta a for loop for counting numbers
+            {
+                sum += dayTemp[i];
+                if (dayTemp[i] > maxTemp)
+                {
+                    maxTemp = dayTemp[i];
+                }
+                if (dayTemp[i] < minTemp)
+                {
+                    minTemp = dayTemp[i];
+                }
+            }
+            Console.WriteLine(sum / dayTemp.Length);
+            Console.WriteLine(maxTemp);
+            Console.WriteLine(minTemp);
+
+
+        }
+    }
+}
